95c61470285c495615069661f6e5e14e7571762a
Martin Jul <martin@mjul.com>
2023-09-04 21:54:02 +0200

    Add skeleton fizzbuzz example

--- src/examples.rs
+++ src/examples.rs
@@ -6,11 +6,7 @@
 //! - it is used by the tests
 //! - it is useful for demonstration purposes
 
-use crate::il::{
-    CompoundStatement, DeclarationsExpr, Expression, ExpressionList, Factor, Id, IdentifierList,
-    NonEmptyVec, ProcedureStatement, ProgramExpr, SimpleExpression, Statement,
-    SubprogramDeclarations, Term,
-};
+use crate::il::{AssignmentStatement, CompoundStatement, DeclarationsExpr, Expression, ExpressionList, Factor, Id, IdentifierList, NonEmptyVec, ProcedureStatement, ProgramExpr, SimpleExpression, StandardType, Statement, SubprogramDeclarations, Term, Type, VarDeclaration, Variable};
 
 const HELLO_WORLD_PAS: &'static str = r#"
             program helloWorld(output);
@@ -33,8 +29,72 @@ pub(crate) fn hello_world() -> ProgramExpr {
                 NonEmptyVec::new(vec![Expression::simple(SimpleExpression::term(
                     Term::factor(Factor::string("Hello, World!")),
                 ))])
-                .unwrap(),
+                    .unwrap(),
             ),
         ))]),
+    )
+}
+
+// Since we don't have for loops in our Pascal dialect, we use a while loop.
+const FIZZBUZZ_PAS: &'static str = r#"
+        program fizzbuzz(output);
+
+        var
+          i: integer;
+
+        begin
+          i := 1;
+          while i <= 100 do
+            if i mod 15 = 0 then
+              writeln('FizzBuzz')
+            else if i mod 3 = 0 then
+              writeln('Fizz')
+            else if i mod 5 = 0 then
+              writeln('Buzz')
+            else
+              writeln(i);
+        end.
+    "#;
+
+
+/// Creates a program expression for the Hello, World! program.
+pub(crate) fn fizzbuzz() -> ProgramExpr {
+    let write_ln_term = |term|
+        Statement::procedure(ProcedureStatement::with_params(
+            Id::new_from_str("writeLn").unwrap(),
+            ExpressionList::new(
+                NonEmptyVec::new(vec![Expression::simple(SimpleExpression::term(
+                    term,
+                ))])
+                    .unwrap(),
+            ),
+        ));
+    let write_ln_str = |str|
+        write_ln_term(Term::factor(Factor::string(str)));
+    let assign_int = |id, value|
+        Statement::assignment(
+            AssignmentStatement::new(
+                Variable::id(Id::new_from_str(id).unwrap()),
+                Expression::simple(
+                    SimpleExpression::term(Term::factor(Factor::number(value))))));
+
+    ProgramExpr::new(
+        Id::new_from_str("fizzbuzz").unwrap(),
+        IdentifierList::new(NonEmptyVec::single(Id::new_from_str("output").unwrap())),
+        DeclarationsExpr::new(
+            vec![VarDeclaration::new(
+                IdentifierList::new(NonEmptyVec::new(vec![Id::new_from_str("i").unwrap()]).unwrap()),
+                Type::standard(StandardType::Integer))]),
+        SubprogramDeclarations::empty(),
+        CompoundStatement::new(
+            vec![
+                assign_int("i", 1),
+                // TODO: while do
+                // TODO: if then else
+                write_ln_str("FizzBuzz"),
+                write_ln_str("Fizz"),
+                write_ln_str("Buzz"),
+                write_ln_term(Term::factor(Factor::id(Id::new_from_str("i").unwrap()))),
+            ]),
     )
 }
